b7c0b72a3672dbf7cd6d40cd4c07e0017669851b
Kai Stevens <kai.stevens97@gmail.com>
2021-11-28 20:07:21 +0000

    added function for playing a single round of rps

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 console.log("Hello World!");
 
-/* computerPLay function
+/* computerPlay function
     randomly return either rock, paper, or scissors
 
     create var for a random int
@@ -26,3 +26,70 @@ function computerPlay(){
         return 'Scissors';
     }
 }
+
+/* singleRound function
+    function will play one round of rps
+
+    the basic logic for a round of rps
+
+    i.e. -> r beats s, p beat r, 
+
+*/
+
+function singleRound(playerSelection, computerSelection){
+
+    // Makes the function case insensitive
+    playerSelection = playerSelection.toLowerCase();
+
+    // Rock
+    if(playerSelection === 'rock'){
+        if(computerSelection === 'Rock'){
+            return 'It is a draw! Rock ties Rock!';
+        }
+        
+        else if(computerSelection === 'Paper'){
+            return 'You lose! Paper beats Rock!';
+        }
+
+        else if(computerSelection === 'Scissors'){
+            return 'You win! Rock beats Scissors';
+        }
+    }
+
+    // Paper
+    else if(playerSelection === 'paper'){
+        if(computerSelection === 'Paper'){
+            return 'It is a draw! Paper ties Paper!';
+        }
+        
+        else if(computerSelection === 'Scissors'){
+            return 'You lose! Scissors beats Paper!';
+        }
+
+        else if(computerSelection === 'Rock'){
+            return 'You win! Paper beats Rock!';
+        }
+    }
+
+    // Scissors
+    else if(playerSelection === 'scissors'){
+        if(computerSelection === 'Scissors'){
+            return 'It is a draw! Scissors ties Scissors!';
+        }
+        
+        else if(computerSelection === 'Rock'){
+            return 'You lose! Rock beats Scissors!';
+        }
+
+        else if(computerSelection === 'Paper'){
+            return 'You win! Scissors beats Paper!';
+        }
+    }
+
+}
+
+// Testing the singleRound function
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(singleRound(playerSelection, computerSelection));
